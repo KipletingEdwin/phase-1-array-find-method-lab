720c973155cea2b936a8d67dde6f5c094b129816
Kipleting <kipletingedwin4@gmail.com>
2022-06-12 18:11:06 +0300

    commit tree bb23267e14cc7d3587d00f88b951fcc48df66815

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+//writing a function
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+  
+  function superbowlWin(record) {
+    function winner(year) {
+        return year.result === "W";
+    }
+    const theWinningYear = record.find(winner)
+    if 
+    (theWinningYear) {
+        return theWinningYear.year
+    }
+}
